0383ac562a8d9884a533df68f8684d5f6cb4b2a7
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-02-23 09:51:40 -0800

    small cleanup
    - Vue.component init inside install.js
    - Export/Reference the right components array
    - Have one .eslintrc

--- template/src/install.js
+++ template/src/install.js
@@ -1,6 +1,10 @@
 import each from 'lodash/each';
-import components from './main';
+import { components } from './main';
 
 each(components, (component, name) => {
-  Vue.component(name, component);
+  if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.component(name, components[name]);
+  } else {
+    Vue.component(name, component);
+  }
 });
--- template/src/main.js
+++ template/src/main.js
@@ -1,14 +1,7 @@
 import HelloWorld from './components/HelloWorld.vue';
 
-const components = {
+export const components = {
   HelloWorld,
 };
-
-Object.keys(components).forEach((name) => {
-  // in browsers ~
-  if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.component(name, components[name]);
-  }
-});
 
 export default HelloWorld;
