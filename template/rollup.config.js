7182763b577ca76f58525b62b2a6579429e4520e
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-02-23 10:06:18 -0800

    add unminified umd build

--- template/rollup.config.js
+++ template/rollup.config.js
@@ -40,21 +40,30 @@ if (vueConfig.standalone) {
       browser: true,
     }),
     commonjs(),
-    uglify(),
   ].concat(options.plugins);
 
-  rollup(options).then(bundle => bundle.write({
+  let promise = Promise.resolve();
+
+  promise.then(() => rollup(options).then(bundle => bundle.write({
     format: 'umd',
-    file: `dist/${pack.name}.min.js`,
+    file: `dist/${pack.name}.umd.js`,
     name: camelcase(pack.name),
   })).then(() => {
+    options.plugins.push(uglify())
+
+    return rollup(options).then(bundle => bundle.write({
+      format: 'umd',
+      file: `dist/${pack.name}.min.js`,
+      name: camelcase(pack.name),
+    }));
+  }).then(() => {
     console.log(chalk.cyan('Build complete.\n'))
   }).catch(err => {
     console.log(
-      chalk.red('Build Failed.\n'),
-      chalk.yellow(err)
+      chalk.yello('Build Failed.\n'),
+      chalk.red(err)
     )
-  });
+  }));
 }
 
 export default config;
